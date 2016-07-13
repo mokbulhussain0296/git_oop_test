ddbaf2631273846ec2477b022eefeb5ded05eacd
mokbul <mokbulhussain2@gmail.com>
2016-07-13 12:20:29 +0600

    implement construct

--- classes/BankAccount.php
+++ classes/BankAccount.php
@@ -5,9 +5,14 @@ class BankAccount implements IfaceBankAccount
 
     private $balance = null;
 
+     public $value=0;
+
     public function __construct(Money $openingBalance)
     {
-        $this->balance = $openingBalance;
+       
+        $this->balance = $openingBalance->value();
+
+        $this->value=$this->balance;
     }
 
     public function balance()
